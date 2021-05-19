212602e6f2a0b8ba198cfe4741644c937081485d
Jon Seager <jon@sgrs.uk>
2021-05-19 16:03:35 +0100

    Initial Commit

---
+++ src/charm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Copyright 2021 Jon Seager
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class HelloOperatorCharm(CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.start, self._on_start)
+
+    def _on_start(self, event):
+        self.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":
+    main(HelloOperatorCharm)
